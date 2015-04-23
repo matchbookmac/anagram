757e9775b450acde464490b98758fe71e30f370e
Ian MacDonald & steven francisco <student@epicodus.com>
2015-04-23 14:18:45 -0700

    return true if word provided is an anagram of any of the other items provided.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,17 @@
+require('pry')
+
 class String
   define_method(:anagram) do |words|
-    word = self.split(//).sort!
-    words = words.split(//).sort!
-    word.eql?(words)
+    word = self.split(//).sort!()
+    words = words.split(/, /).sort!()
+    anagrams = false
+
+    words.each() do |item|
+      item = item.split(//).sort!()
+      if(word.length().==(item.length()) && (word.eql?(item)))
+        anagrams = true
+      end
+    end
+    anagrams
   end
 end
